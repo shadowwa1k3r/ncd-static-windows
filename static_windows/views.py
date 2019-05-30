e67f77eddd67fc841ccb1d244efe119c96635293
MurodilloUrakov <murakov1996@gmail.com>
2019-05-30 17:29:23 +0500

    sdadsasdas

--- static_windows/views.py
+++ static_windows/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.views.generic import View, ListView, TemplateView
 
 class IndexView(TemplateView):
-    template_name = 'base.html'
+    template_name = 'index.html'
 
