584fe350890d2aa196357d01cdda6b457b5291ae
shadowwa1k3r <ergash1994@gmail.com>
2019-05-31 10:07:06 +0500

    wtf

--- api/about/views.py
+++ api/about/views.py
@@ -1,5 +1,6 @@
-from rest_framework.generics import CreateAPIView
-
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from about.models import About
 from .serializers import AboutSerializer
 
@@ -7,3 +8,17 @@ from .serializers import AboutSerializer
 class AboutCreate(CreateAPIView):
     queryset = About.objects.all()
     serializer_class = AboutSerializer
+
+
+class AboutUpdate(UpdateAPIView):
+    queryset = About.objects.all()
+    serializer_class = AboutSerializer
+    lookup_url_kwarg = 'id'
+
+
+class AboutDelete(APIView):
+    def post(self, request):
+        about_id = request.data.get('about_id')
+        about = About.objects.get(id=about_id)
+        about.delete()
+        return Response(status=200)
--- settings/settings.py
+++ settings/settings.py
@@ -80,9 +80,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         # local
-        'NAME': 'ncdCMS',
-        'USER': 'murakov',
-        'PASSWORD': '12345',
+        'NAME': 'ncd',
+        'USER': 'ncduser',
+        'PASSWORD': '1',
     }
 }
 
