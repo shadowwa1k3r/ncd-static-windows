98fe2e19a86b3d05bffaed79384dbd02c32e0213
shadowwa1k3r <ergash1994@gmail.com>
2019-06-08 12:13:41 +0500

    holy_shit

--- api/edu/urls.py
+++ api/edu/urls.py
@@ -5,6 +5,6 @@ from api.edu.views import EduCreate, EduUpdate, EduDelete, DocumentDelete
 urlpatterns = [
     path('create/', EduCreate.as_view(), name='edu-create-api'),
     path('delete/<int:id>', EduDelete.as_view(), name='edu-delete-api'),
-    path('document/delete/<int:id>', DocumentDelete.as_view(), name='document-delete-api'),
+    path('document/delete/<int:id>', DocumentDelete.as_view(), name='edu-document-delete-api'),
     path('update/<int:id>', EduUpdate.as_view(), name='edu-update-api'),
 ]
